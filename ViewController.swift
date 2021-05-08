e00afadd26ea92dbceb1f57645f3a5eb466ded92
jkmoore <jkm9@pdx.edu>
2021-05-08 13:14:42 +0900

    Create basic view controller structure, collapsible table cells, sample text

--- ViewController.swift
+++ ViewController.swift
@@ -7,13 +7,200 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Section {
+    let title: String
+    let options: [String]
+    var isOpened: Bool = false
+    
+    init(title: String, options: [String], isOpened: Bool = false) {
+        self.title = title
+        self.options = options
+        self.isOpened = isOpened
+    }
+}
+
+class JournalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var sections = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Journal"
+        
+        sections = [
+            Section(title: "May 6", options: [" Here is a long journal entry. We want to be able to see everything."]),
+            Section(title: "May 7", options: [" Second entry"]),
+            Section(title: "May 8", options: [" Third entry"])
+        ]
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        if section.isOpened {
+            return section.options.count + 1
+        }
+        else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+        }
+        else {
+            
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        return cell
     }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+    }
 }
 
+class MedicineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var sections = [Section]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Medicines"
+        
+        sections = [
+            Section(title: "Medicine A", options: [" Started May 6, 2021", " Take once a day before bed"]),
+            Section(title: "Medicine B", options: [" Started May 7, 2021", " Apply every morning and night"]),
+            Section(title: "Medicine C", options: [" Started May 8, 2021", " Use to treat allergies"])
+        ]
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        if section.isOpened {
+            return section.options.count + 1
+        }
+        else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+        }
+        else {
+            
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+    }
+}
+
+class ClinicViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var sections = [Section]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Clinics"
+        
+        sections = [
+            Section(title: "Clinic A", options: [" Address: 1234 NW Clinic St Portland, OR", " (503) 123-4567"]),
+            Section(title: "Hospital B", options: [" Address: 5678 SE Hospital St Portland, OR", " (971) 765-4321"]),
+            Section(title: "Dentist C", options: [" Address: 9876 SW Dentist St Portland, OR", " (503) 321-7654"])
+        ]
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        if section.isOpened {
+            return section.options.count + 1
+        }
+        else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+        }
+        else {
+            
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+    }
+}
